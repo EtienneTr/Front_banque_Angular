a1f971bdf1fdd56866fc596f6c60b129d81b3681
EtienneT <tienou42@gmail.com>
2017-03-09 11:19:32 +0100

    Transfert OK + menu

--- src/app/pages/transfer/transfer.component.ts
+++ src/app/pages/transfer/transfer.component.ts
@@ -18,6 +18,7 @@ export class TransferComponent implements OnInit {
   //authService: AuthService;
   transfertForm: FormGroup;
   error = "";
+  success = "";
   accounts: Object;
 
   radio1=true;
@@ -63,8 +64,11 @@ export class TransferComponent implements OnInit {
   ngOnInit(){
 
     this.transfertForm = this.formBuilder.group({
-      accountFrom: ['', Validators.required],
-      accountTo: ['', Validators.required]
+      fromAccount: ['0', Validators.required],
+      isAccount: ['in', Validators.required],
+      inAccount: ['0', Validators.required],
+      outAccount: ['0', Validators.required],
+      amount: ['', Validators.required]
     });
   }
 
@@ -80,22 +84,36 @@ export class TransferComponent implements OnInit {
     }
   }
 
-  onLoginSubmit(){
-    console.log(this.transfertForm);
+  onTransferSubmit(){
     let formValues = this.transfertForm.value;
-    /*let mail = formValues.userName;
-    let pass = formValues.userPass;
 
-    this.authService.loginUser(mail, pass)
-      .subscribe(result => {
-          if(result === true){
-            this.router.navigate(['/']);
-          } else {
-            this.error = "Mail ou mot de passe incorrecte";
-          }},
+    let fromAccount = formValues.fromAccount;
+    let isAccount = formValues.isAccount;
+    let toAccount = "";
+    if(isAccount === "in"){
+      toAccount = formValues.inAccount;
+    }
+    if(isAccount === "out"){
+      toAccount = formValues.outAccount;
+    }
+    let amount = formValues.amount;
+
+    if(fromAccount !== toAccount){
+
+      this.userService.transfertAccounts(fromAccount, toAccount, amount, this.user.token)
+        .subscribe(result =>{
+          if(result && result.status === 200){
+            this.success = "Virement effectué avec succès !";
+          }
+        },
         error => {
-          this.error = "Mail ou mot de passe incorrecte";
-        });*/
+          this.error = "Le virement n'a pas pu s'effectuer. Veuilelz vérifier vos informations.";
+        });
+
+    } else {
+      this.error = "Impossible d'effectuer un virement sur le même compte";
+    }
+
 
   }
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -42,6 +42,8 @@ export class UserService {
     return this.http.put(createUrl, bodyString, options).map((response: Response) => response.json());
   }
 
+
+  //ACCOUNTS
   getAccountDetails(account: string, token: string){
     let createUrl = this.baseUrl + 'account/' + account;
     let headers    = new Headers({ 'Content-Type': 'application/json', 'x-access-token': token });
@@ -56,6 +58,15 @@ export class UserService {
     let options    = new RequestOptions({ headers: headers });
 
     return this.http.get(createUrl, options).map((response: Response) => response.json());
+  }
+
+  transfertAccounts(fromAccount: string, toAccount: string, amount: number, token: string){
+    let transfertUrl = this.baseUrl + 'account/transfer/' + fromAccount + '/' + toAccount;
+    let bodyString = JSON.stringify({amount: amount});
+    let headers    = new Headers({ 'Content-Type': 'application/json', 'x-access-token': token });
+    let options    = new RequestOptions({ headers: headers });
+
+    return this.http.post(transfertUrl, bodyString, options).map((response: Response) => response.json());
   }
 
 }
